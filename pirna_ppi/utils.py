ea0d9b46452f0ddd1359de32adb6d3351135d7f1
luxiangze <gyk666@email.swu.edu.cn>
2025-12-15 17:42:10 +0800

    feat: Add utility module with command execution helper function
    Added the utils.py module, which includes the run_cmd function to execute commands and check if the output file exists, thereby avoiding the re-execution of tasks that have already been completed.

---
+++ pirna_ppi/utils.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import subprocess
+
+def run_cmd(cmd: str, outputs: list[str]):
+    outputs = [Path(o) for o in outputs]
+
+    if all(o.exists() for o in outputs):
+        print("⏭️ output exists, skip")
+        return
+
+    subprocess.run(cmd, shell=True, check=True)
